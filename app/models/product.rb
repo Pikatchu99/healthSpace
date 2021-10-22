7a2d187418aaa2d5c6fe2e1a567fb69785b69e09
Pikatchu99 <yemalin.agbangla@epitech.eu>
2021-10-22 09:32:58 +0100

    seed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
     validates :name, presence: true,  length: {minimum: 4, maximum: 150}
     validates :description, length: {minimum: 50, maximum: 500}
-    validates :first_image, presence: true
-    validates :second_image, presence: true
+    # validates :first_image, presence: true
+    # validates :second_image, presence: true
 
     belongs_to :pharmacy
     mount_uploader :first_image, FirstImageUploader
